c439da601f055400485046d33179fc139d1fd62e
Youssef04544 <youssefbenyedder04@gmail.com>
2021-11-21 08:45:56 +0100

    Finished all the styling needed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import React from "react";
+import SideBarItem from "./SideBarItem";
 import Slider from "./Slider.js";
 
 function App() {
   return (
     <div className='container'>
       <div className='main-image'></div>
-      <div className='sidebar'>Sidebar</div>
+      <div className='sidebar'>
+        <SideBarItem />
+        <SideBarItem />
+      </div>
       <Slider />
     </div>
   );
---
+++ src/SideBarItem.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SideBarItem() {
+  return <button className='sidebar-item'>Sidebar Item</button>;
+}
