4e82a56e2bb1ee7d7207b0665772696f312c8095
Youssef04544 <youssefbenyedder04@gmail.com>
2021-11-21 08:33:41 +0100

    Prepared the image and slider bar and basic components with CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Slider from "./Slider.js";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='container'>
+      <div className='main-image'></div>
+      <div className='sidebar'>Sidebar</div>
+      <Slider />
+    </div>
+  );
 }
 
 export default App;
